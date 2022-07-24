e313226a417b32e887c0c0b149c7640aa13d3311
Sondre Agledahl <sondre@sondre.io>
2022-07-24 15:25:01 +0100

    Transforms

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,6 +1,7 @@
 #include <Dolan.h>
 
 #include "imgui/imgui.h"
+#include "glm/gtc/matrix_transform.hpp"
 
 class ExampleLayer : public Dolan::Layer
 {
@@ -34,10 +35,10 @@ public:
 		m_SquareVa.reset(Dolan::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f,
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f,
 		};
 
 		std::shared_ptr<Dolan::VertexBuffer> squareVb;
@@ -59,6 +60,7 @@ public:
 			layout(location = 1) in vec4 a_Color;		
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -67,7 +69,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -94,13 +96,14 @@ public:
 			layout(location = 0) in vec3 a_Position;		
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;			
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -143,7 +146,18 @@ public:
 		m_Camera.SetRotation(m_CameraRot);
 
 		Dolan::Renderer::BeginScene(m_Camera);
-		Dolan::Renderer::Submit(m_BlueShader, m_SquareVa);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f, 0.1f, 0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+
+				Dolan::Renderer::Submit(m_BlueShader, m_SquareVa, transform);
+			}
+		}
 
 		Dolan::Renderer::Submit(m_Shader, m_VertexArray);
 		
