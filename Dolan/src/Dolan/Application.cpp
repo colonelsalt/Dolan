97b378643a5c0476c62ba4e6bd214c9f1b37f506
Sondre Agledahl <sondre@sondre.io>
2022-07-28 07:59:28 +0100

    Correctly resizing window

--- Dolan/src/Dolan/Application.cpp
+++ Dolan/src/Dolan/Application.cpp
@@ -44,6 +44,7 @@ namespace Dolan {
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(OnWindowResize));
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
 		{
@@ -61,14 +62,16 @@ namespace Dolan {
 			Timestep timestep = time - m_LastFrameTime;
 			m_LastFrameTime = time;
 
-			for (Layer* layer : m_LayerStack)
-				layer->OnUpdate(timestep);
-			
+			if (!m_Minimized)
+			{
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);	
+			}
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
 				layer->OnImGuiRender();
 			m_ImGuiLayer->End();
-
+			
 			m_Window->OnUpdate();
 		}
 	}
@@ -77,5 +80,19 @@ namespace Dolan {
 	{
 		m_Running = false;
 		return true;
+	}
+
+	bool Application::OnWindowResize(WindowResizeEvent& e)
+	{
+		if (e.GetWidth() == 0 || e.GetHeight() == 0)
+		{
+			m_Minimized = true;
+			return false;
+		}
+
+		m_Minimized = false;
+		Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+		return false;
 	}
 }
--- Dolan/src/Dolan/Renderer/RenderCommand.h
+++ Dolan/src/Dolan/Renderer/RenderCommand.h
@@ -12,6 +12,11 @@ namespace Dolan {
 			s_RendererApi->Init();
 		}
 
+		inline static void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+		{
+			s_RendererApi->SetViewport(x, y, width, height);
+		}
+
 		inline static void SetClearColor(const glm::vec4& color)
 		{
 			s_RendererApi->SetClearColor(color);
