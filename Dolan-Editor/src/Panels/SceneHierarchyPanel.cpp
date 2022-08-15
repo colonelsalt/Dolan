9ce7c8ee5d91bbfb3cf31d5b1cc816a48270517d
Sondre Agledahl <sondre@sondre.io>
2022-08-15 08:38:22 +0100

    Editor properties panel

--- Dolan-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Dolan-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -3,6 +3,7 @@
 #include "Dolan/Scene/Components.h"
 
 #include <imgui/imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Dolan {
 
@@ -27,6 +28,18 @@ namespace Dolan {
 			DrawEntityNode(entity);
 		});
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectedEntity = {};
+
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectedEntity)
+		{
+			DrawComponents(m_SelectedEntity);
+		}
+
+
 		ImGui::End();
 	}
 
@@ -38,15 +51,45 @@ namespace Dolan {
 		if (m_SelectedEntity == entity)
 			flags |= ImGuiTreeNodeFlags_Selected;
 		
-		bool isExanped = ImGui::TreeNodeEx((void*)(uint64_t)(uint32_t)entity, flags, tag.c_str());
+		bool isExpanded = ImGui::TreeNodeEx((void*)(uint64_t)(uint32_t)entity, flags, tag.c_str());
 		if (ImGui::IsItemClicked())
 		{
 			m_SelectedEntity = entity;
 		}
 
-		if (isExanped)
+		if (isExpanded)
 		{
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			std::string& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy(buffer, tag.c_str());
+
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				glm::mat4& transform = entity.GetComponent<TransformComponent>().Transform;
+
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
+
 		}
 	}
 
